e5891f8914f60dfd0e350670f3654e4f154ba233
Niklas Lundberg <niklas.lundberg@jayway.com>
2014-04-10 13:53:45 +0200

    Added GetVariable.

--- src/Arbor.X.Core/BuildVariables/BuildVariableExtensions.cs
+++ src/Arbor.X.Core/BuildVariables/BuildVariableExtensions.cs
@@ -11,5 +11,11 @@ namespace Arbor.X.Core.BuildVariables
             return buildVariables.Any(
                 bv => bv.Key.Equals(key, StringComparison.InvariantCultureIgnoreCase));
         }
+
+        public static IVariable GetVariable(this IReadOnlyCollection<IVariable> buildVariables, string key)
+        {
+            return buildVariables.Single(
+                bv => bv.Key.Equals(key, StringComparison.InvariantCultureIgnoreCase));
+        }
     }
 }
