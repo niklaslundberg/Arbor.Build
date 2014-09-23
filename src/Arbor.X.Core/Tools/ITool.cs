f63cc9f2c20315d28e75c40de97401e0df52f40c
Niklas Lundberg <niklas.lundberg@jayway.com>
2014-09-23 13:18:55 +0200

    Added docs to ITool.

--- src/Arbor.X.Core/Tools/ITool.cs
+++ src/Arbor.X.Core/Tools/ITool.cs
@@ -6,8 +6,18 @@ using Arbor.X.Core.Logging;
 
 namespace Arbor.X.Core.Tools
 {
+    /// <summary>
+    /// ITool represents an arbitrary tool than can execute basically anything. 
+    /// </summary>
     public interface ITool
     {
+        /// <summary>
+        /// Implementations should handle errors and write to the log with the passed ILogger instance. If it fails it should return an ExitCode with a code not 0 and. 
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="buildVariables"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>ExitCode, value 0 meaning success; any other value failure</returns>
         Task<ExitCode> ExecuteAsync(ILogger logger, IReadOnlyCollection<IVariable> buildVariables, CancellationToken cancellationToken);
     }
 }
