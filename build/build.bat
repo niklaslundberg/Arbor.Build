13a8b129d60f83e75b73fe311776b84ec7b31e7b
Niklas Lundberg <niklas.lundberg@jayway.com>
2014-09-23 23:03:51 +0200

    Default build params.

--- build/build.bat
+++ build/build.bat
@@ -1,10 +1,12 @@
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 SET Arbor.X.Vcs.Branch.Name=develop
 SET Arbor.X.Tools.External.MSpec.Enabled=false
+SET Arbor.X.NuGet.Package.Artifacts.Suffix=
+SET Arbor.X.NuGet.Package.Artifacts.BuildNumber.Enabled=
 
 SET Version.Major=0
 SET Version.Minor=1
 SET Version.Patch=18
-SET Version.Build=11
+SET Version.Build=15
 
 CALL "%~dp0\Build.exe"
