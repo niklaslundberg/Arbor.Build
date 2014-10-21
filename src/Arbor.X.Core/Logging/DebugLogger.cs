a0d7cf71e2000063ebefab7f57a96bfc6683de1f
Niklas Lundberg <niklas.lundberg@jayway.com>
2014-10-21 17:01:12 +0200

    Added DebugLogger.

---
+++ src/Arbor.X.Core/Logging/DebugLogger.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace Arbor.X.Core.Logging
+{
+    public class DebugLogger : ILogger
+    {
+        readonly ILogger _logger;
+
+        public DebugLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteError(string message, string prefix = null)
+        {
+            if (Debugger.IsAttached && string.IsNullOrWhiteSpace(message))
+            {
+                Debugger.Break();
+                return;
+            }
+            Debug.WriteLine(message, TraceEventType.Error.ToString());
+            _logger.WriteError(message, prefix);
+        }
+
+        public void Write(string message, string prefix = null)
+        {
+            Debug.WriteLine(message, TraceEventType.Information.ToString());
+            _logger.Write(message, prefix);
+        }
+
+        public void WriteWarning(string message, string prefix = null)
+        {
+            if (Debugger.IsAttached && string.IsNullOrWhiteSpace(message))
+            {
+                Debugger.Break();
+                return;
+            }
+            Debug.WriteLine(message, TraceEventType.Warning.ToString());
+            _logger.WriteWarning(message, prefix);
+        }
+
+        public void WriteVerbose(string message, string prefix = null)
+        {
+            Debug.WriteLine(message, TraceEventType.Verbose.ToString());
+            _logger.WriteVerbose(message, prefix);
+        }
+
+        public LogLevel LogLevel { get; set; }
+        public void WriteDebug(string message, string prefix = null)
+        {
+            Debug.WriteLine(message, "DEBUG");
+            _logger.WriteDebug(message, prefix);
+        }
+    }
+}
