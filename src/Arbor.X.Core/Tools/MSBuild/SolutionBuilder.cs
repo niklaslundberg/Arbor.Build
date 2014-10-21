3c0f2c25c6adb294ef4e4b8c3a3662d1ba9d68ec
Niklas Lundberg <niklas.lundberg@jayway.com>
2014-10-21 17:03:24 +0200

    Removed unused reference.

--- src/Arbor.X.Core/Tools/MSBuild/SolutionBuilder.cs
+++ src/Arbor.X.Core/Tools/MSBuild/SolutionBuilder.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Arbor.Aesculus.Core;
 using Arbor.X.Core.BuildVariables;
 using Arbor.X.Core.IO;
 using Arbor.X.Core.Logging;
