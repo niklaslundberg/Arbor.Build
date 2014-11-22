9ce10008b27d50371f55aefc12418e6c94bbdce3
Niklas Lundberg <niklas.lundberg@jayway.com>
2014-11-22 17:06:08 +0100

    Started SASS compilation.

---
+++ src/Arbor.X.Core/BuildVariables/WellknownVariables.NodeJs.cs
@@ -0,0 +1,8 @@
+﻿namespace Arbor.X.Core.BuildVariables
+{
+    public partial class WellKnownVariables
+    {
+        [VariableDescription("Node JS exe path")] public const string NodeJsExePath =
+            "Arbor.X.Tools.NodeJS.NodeExePath";
+    }
+}
