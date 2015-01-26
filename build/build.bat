af931163334953d751ddd899ec27145d58d99779
Niklas Lundberg <niklas.lundberg@jayway.com>
2015-01-26 16:25:40 +0100

    Updated bootstrapper to current version.

--- build/build.bat
+++ build/build.bat
@@ -1,19 +1,21 @@
+@ECHO OFF
 SET Arbor.X.Build.Bootstrapper.AllowPrerelease=true
 SET Arbor.X.Tools.External.MSpec.Enabled=false
 SET Arbor.X.NuGet.Package.Artifacts.Suffix=
 SET Arbor.X.NuGet.Package.Artifacts.BuildNumber.Enabled=
 SET Arbor.X.Log.Level=Debug
 SET Arbor.X.NuGetPackageVersion=
-SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled=true
+SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled=false
 SET Arbor.X.Build.VariableOverrideEnabled=true
+SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=true
 
-SET Version.Major=0
-SET Version.Minor=1
-SET Version.Patch=0
-SET Version.Build=15
+SET Version.Major=1
+SET Version.Minor=0
+SET Version.Patch=6
+SET Version.Build=2
 
-SET Arbor.X.Vcs.Branch.Name=refs/heads/develop-0.1.26
-REM SET Arbor.X.Vcs.Branch.Name=develop
+REM SET Arbor.X.Vcs.Branch.Name=refs/heads/develop-0.1.26
+SET Arbor.X.Vcs.Branch.Name=release-1.0
 
 CALL "%~dp0\Build.exe"
 
@@ -28,3 +30,9 @@ SET Arbor.X.Log.Level=
 SET Arbor.X.NuGetPackageVersion=
 SET Arbor.X.Vcs.Branch.Name.Version.OverrideEnabled
 SET Arbor.X.VariableOverrideEnabled=
+SET Arbor.X.Artifacts.CleanupBeforeBuildEnabled=
+
+SET Version.Major=
+SET Version.Minor=
+SET Version.Patch=
+SET Version.Build=
