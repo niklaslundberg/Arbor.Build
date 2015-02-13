d21cc756b1668a541d333ee4c7e7e1cf634189a9
Niklas Lundberg <niklas.lundberg@jayway.com>
2015-02-13 16:29:33 +0100

    Added enumerable extensions.

---
+++ src/Arbor.X.Core/EnumerableExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Arbor.X.Core
+{
+    public static class EnumerableExtensions
+    {
+        public static IReadOnlyCollection<T> ToReadOnlyCollection<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null)
+            {
+                throw new ArgumentNullException("enumerable");
+            }
+
+            if (enumerable is List<T>)
+            {
+                return new ReadOnlyCollection<T>((List<T>)enumerable);
+            }
+
+            return new ReadOnlyCollection<T>(enumerable.ToList());
+        }
+
+        public static IReadOnlyCollection<T> SafeToReadOnlyCollection<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null)
+            {
+                return Enumerable.Empty<T>().ToReadOnlyCollection();
+            }
+
+            if (enumerable is List<T>)
+            {
+                return new ReadOnlyCollection<T>((List<T>)enumerable);
+            }
+
+            return new ReadOnlyCollection<T>(enumerable.ToList());
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            if (enumerable == null)
+            {
+                throw new ArgumentNullException("enumerable");
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException("action");
+            }
+
+            foreach (var item in enumerable)
+            {
+                var current = item;
+
+                action(current);
+            }
+        }
+    }
+}
