3bd9ea87a8bcd435d786b2d970df41c0a4cc2bb9
Niklas Lundberg <niklas.lundberg@milou.se>
2015-03-19 15:09:47 +0100

    Fixed unit testfinder bug where native dlls caused exception.

--- src/Arbor.X.Core/Tools/Testing/UnitTestFinder.cs
+++ src/Arbor.X.Core/Tools/Testing/UnitTestFinder.cs
@@ -254,6 +254,12 @@ namespace Arbor.X.Core.Tools.Testing
                 _logger.WriteDebug(string.Format("Could not load assembly '{0}'. Ignoring.", dllFile.FullName));
                 return null;
             }
+// ReSharper disable once UnusedVariable
+            catch (BadImageFormatException ex)
+            {
+                _logger.WriteDebug(string.Format("Could not load assembly '{0}'. Ignoring.", dllFile.FullName));
+                return null;
+            }
         }
     }
 }
