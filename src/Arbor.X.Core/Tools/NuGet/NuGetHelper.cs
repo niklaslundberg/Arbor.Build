66f91735b14d4370cf09e30918f839b6b87836fd
Niklas Lundberg <info@nlundberg.se>
2015-06-05 12:23:29 +0200

    Added fallback DNS for NuGet.

--- src/Arbor.X.Core/Tools/NuGet/NuGetHelper.cs
+++ src/Arbor.X.Core/Tools/NuGet/NuGetHelper.cs
@@ -4,6 +4,9 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Arbor.X.Core.Logging;
+using File = Alphaleonis.Win32.Filesystem.File;
+using FileInfo = Alphaleonis.Win32.Filesystem.FileInfo;
+using Path = Alphaleonis.Win32.Filesystem.Path;
 
 namespace Arbor.X.Core.Tools.NuGet
 {
@@ -21,25 +24,38 @@ namespace Arbor.X.Core.Tools.NuGet
             var baseDir = AppDomain.CurrentDomain.BaseDirectory;
             var targetFile = Path.Combine(baseDir, "nuget.exe");
 
-            const int maxRetries = 3;
-            
+            const int maxRetries = 4;
+
+            var currentExePath = new FileInfo(targetFile);
+
             if (!File.Exists(targetFile))
             {
+                var parentExePath = Path.Combine(currentExePath.Directory.Parent.FullName, currentExePath.Name);
+                if (Alphaleonis.Win32.Filesystem.File.Exists(parentExePath))
+                {
+                    _logger.Write($"Found NuGet in path '{parentExePath}', skipping download");
+                    return parentExePath;
+                }
+
+                _logger.Write($"'{targetFile}' does not exist, will try to download from nuget.org");
+
                 for (int i = 0; i < maxRetries; i++)
                 {
                     try
                     {
-                        await DownloadNuGetExeAsync(baseDir, targetFile, cancellationToken);
+                        string nugetExeUri = i < 2 ? "https://nuget.org/nuget.exe" : "https://www.nuget.org/nuget.exe";
+
+                        await DownloadNuGetExeAsync(baseDir, targetFile, nugetExeUri, cancellationToken);
 
                         return targetFile;
                     }
                     catch (Exception ex)
-                    {   
+                    {
                         _logger.WriteError(string.Format("Attempt {1}. Could not download nuget.exe. {0}", ex, i + 1));
                     }
 
                     const int waitTimeInSeconds = 2;
-                    
+
                     _logger.Write(string.Format("Waiting {0} seconds to try again", waitTimeInSeconds));
 
                     await Task.Delay(TimeSpan.FromSeconds(waitTimeInSeconds), cancellationToken);
@@ -49,11 +65,9 @@ namespace Arbor.X.Core.Tools.NuGet
             return targetFile;
         }
 
-        async Task DownloadNuGetExeAsync(string baseDir, string targetFile, CancellationToken cancellationToken)
+        async Task DownloadNuGetExeAsync(string baseDir, string targetFile, string nugetExeUri, CancellationToken cancellationToken)
         {
             var tempFile = Path.Combine(baseDir, string.Format("nuget.exe.{0}.tmp", Guid.NewGuid()));
-
-            const string nugetExeUri = "https://nuget.org/nuget.exe";
 
             _logger.WriteVerbose(string.Format("Downloading {0} to {1}", nugetExeUri, tempFile));
             try
@@ -64,10 +78,9 @@ namespace Arbor.X.Core.Tools.NuGet
                     {
                         using (var fs = new FileStream(tempFile, FileMode.Create))
                         {
-                            await stream.CopyToAsync(fs,4096,cancellationToken);
+                            await stream.CopyToAsync(fs, 4096, cancellationToken);
                         }
                     }
-
                 }
             }
             finally
