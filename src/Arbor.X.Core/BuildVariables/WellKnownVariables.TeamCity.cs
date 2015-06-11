71c142b90f21116f77bc96e01cd8e2510e1ee1fb
Niklas Lundberg <info@nlundberg.se>
2015-06-11 13:53:54 +0200

    Improved build agent detection.

--- src/Arbor.X.Core/BuildVariables/WellKnownVariables.TeamCity.cs
+++ src/Arbor.X.Core/BuildVariables/WellKnownVariables.TeamCity.cs
@@ -11,6 +11,9 @@ namespace Arbor.X.Core.BuildVariables
         [VariableDescription("Flag to indiciate if running in TeamCity (calculated)")]
         public static readonly string ExternalTools_TeamCity_IsRunningInTeamCity = "Arbor.X.Tools.External.TeamCity.IsRunningInTeamCity";
 
+        [VariableDescription("TeamCity Version")]
+        public static readonly string ExternalTools_TeamCity_TeamCityVersion = "TEAMCITY_VERSION";
+
         [VariableDescription("TeamCity build configuration name")]
         public static readonly string ExternalTools_TeamCity_BuildConfigurationName = "TEAMCITY_BUILDCONF_NAME";
     }
--- src/Arbor.X.Core/Tools/Cleanup/VariableProviderOrder.cs
+++ src/Arbor.X.Core/Tools/Cleanup/VariableProviderOrder.cs
@@ -2,6 +2,7 @@ namespace Arbor.X.Core.Tools.Cleanup
 {
     public static class VariableProviderOrder
     {
+        public const int Default = 10000;
         public const int Ignored = int.MaxValue;
     }
 }
