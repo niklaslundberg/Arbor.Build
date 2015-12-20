f13c47623d37a7c518ac1f07844be33e35012446
Niklas Lundberg <info@nlundberg.se>
2015-12-20 12:31:36 +0100

    Moved classes to separate files.

--- src/Arbor.X.Core/Tools/MSBuild/WellKnownPlatforms.cs
+++ src/Arbor.X.Core/Tools/MSBuild/WellKnownPlatforms.cs
@@ -1,24 +1,8 @@
-﻿using System;
-
-using JetBrains.Annotations;
-
-namespace Arbor.X.Core.Tools.MSBuild
+﻿namespace Arbor.X.Core.Tools.MSBuild
 {
     public static class WellKnownPlatforms
     {
         // ReSharper disable once InconsistentNaming
         public const string AnyCPU = "Any CPU";
-    }
-
-    public static class Platforms
-    {
-        public static string Normalize([NotNull] string platform)
-        {
-            if (string.IsNullOrWhiteSpace(platform))
-            {
-                throw new ArgumentNullException(nameof(platform));
-            }
-            return platform.Replace(" ", "");
-        }
     }
 }
