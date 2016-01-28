bfb9591500343d12984d9b96190a5f0426660178
Niklas Lundberg <info@nlundberg.se>
2016-01-28 13:45:14 +0100

    Started new input for version number.

--- src/Arbor.X.Core/Tools/Versioning/BuildVersionProvider.cs
+++ src/Arbor.X.Core/Tools/Versioning/BuildVersionProvider.cs
@@ -4,7 +4,14 @@ using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Alphaleonis.Win32.Filesystem;
+
+using Arbor.KVConfiguration.Core;
+using Arbor.KVConfiguration.JsonConfiguration;
+using Arbor.KVConfiguration.Schema;
 using Arbor.X.Core.BuildVariables;
+using Arbor.X.Core.GenericExtensions;
 using Arbor.X.Core.Logging;
 using Arbor.X.Core.Tools.Cleanup;
 
@@ -17,11 +24,10 @@ namespace Arbor.X.Core.Tools.Versioning
     {
         public Task<IEnumerable<IVariable>> GetEnvironmentVariablesAsync(ILogger logger, IReadOnlyCollection<IVariable> buildVariables, CancellationToken cancellationToken)
         {
-            var variables =
-                GetVersionVariables(
-                    buildVariables.Select(item => new KeyValuePair<string, string>(item.Key, item.Value)).ToList());
+            IEnumerable<KeyValuePair<string, string>> variables =
+                GetVersionVariables(buildVariables);
 
-            var environmentVariables = variables
+            List<EnvironmentVariable> environmentVariables = variables
                 .Select(item => new EnvironmentVariable(item.Key, item.Value))
                 .ToList();
 
@@ -29,30 +35,85 @@ namespace Arbor.X.Core.Tools.Versioning
         }
 
         IEnumerable<KeyValuePair<string, string>> GetVersionVariables(
-            IReadOnlyList<KeyValuePair<string, string>> environmentVariables)
+            IReadOnlyList<IVariable> buildVariables)
         {
-            var major =
+
+            var environmentVariables =
+                buildVariables.Select(item => new KeyValuePair<string, string>(item.Key, item.Value)).ToList();
+
+            int major =
                 environmentVariables.Where(item => item.Key == WellKnownVariables.VersionMajor)
                     .Select(item => (int?) int.Parse(item.Value))
-                    .SingleOrDefault() ?? 0;
-            var minor =
+                    .SingleOrDefault() ?? -1;
+            int minor =
                 environmentVariables.Where(item => item.Key == WellKnownVariables.VersionMinor)
                     .Select(item => (int?) int.Parse(item.Value))
-                    .SingleOrDefault() ?? 1;
-            var patch =
+                    .SingleOrDefault() ?? -1;
+            int patch =
                 environmentVariables.Where(item => item.Key == WellKnownVariables.VersionPatch)
                     .Select(item => (int?) int.Parse(item.Value))
-                    .SingleOrDefault() ?? 0;
-            var build =
+                    .SingleOrDefault() ?? -1;
+            int build =
                 environmentVariables.Where(item => item.Key == WellKnownVariables.VersionBuild)
                     .Select(item => (int?) int.Parse(item.Value))
-                    .SingleOrDefault() ?? 0;
+                    .SingleOrDefault() ?? -1;
 
-            var netAssemblyVersion = new Version(major, minor, 0, 0);
-            var fullVersion = new Version(major, minor, patch, build);
-            var fullVersionValue = fullVersion.ToString(fieldCount: 4);
-            var netAssemblyFileVersion = new Version(major, minor, patch, build);
-            
+            string sourceRoot = buildVariables.GetVariable(WellKnownVariables.SourceRoot).ThrowIfEmptyValue().Value;
+
+            var versionFileName = Path.Combine(sourceRoot, "version.json");
+
+            if (File.Exists(versionFileName))
+            {
+                IReadOnlyCollection<KeyValueConfigurationItem> keyValueConfigurationItems = new JsonFileReader(versionFileName).ReadConfiguration();
+
+                var jsonKeyValueConfiguration = new JsonKeyValueConfiguration(keyValueConfigurationItems);
+
+                var majorKey = "major"; //TODO defined major key
+
+                var minorKey = "minor"; //TODO defined minor key
+
+                var patchKey = "patch"; //TODO defined patch key
+
+                var required = new Dictionary<string, string>
+                                   {
+                                       { majorKey, jsonKeyValueConfiguration.ValueOrDefault(majorKey)},
+                                       { minorKey, jsonKeyValueConfiguration.ValueOrDefault(minorKey)},
+                                       { patchKey, jsonKeyValueConfiguration.ValueOrDefault(patchKey)}
+                                   };
+
+                if (required.All(ValidateVersionNumber))
+                {
+                    major = required[majorKey].TryParseInt32().Value;
+                    minor = required[minorKey].TryParseInt32().Value;
+                    patch = required[patchKey].TryParseInt32().Value;
+                }
+            }
+
+            if (major < 0)
+            {
+                major = 0;
+            }
+
+            if (minor < 0)
+            {
+                minor = 0;
+            }
+
+            if (patch < 0)
+            {
+                patch = 0;
+            }
+
+            if (major == 0 && minor == 0 && patch == 0)
+            {
+                minor = 1;
+            }
+
+            Version netAssemblyVersion = new Version(major, minor, 0, 0);
+            Version fullVersion = new Version(major, minor, patch, build);
+            string fullVersionValue = fullVersion.ToString(fieldCount: 4);
+            Version netAssemblyFileVersion = new Version(major, minor, patch, build);
+
             yield return
                 new KeyValuePair<string, string>(WellKnownVariables.NetAssemblyVersion, netAssemblyVersion.ToString(fieldCount: 4));
             yield return
@@ -64,6 +125,23 @@ namespace Arbor.X.Core.Tools.Versioning
             yield return new KeyValuePair<string, string>(WellKnownVariables.VersionBuild, build.ToString(CultureInfo.InvariantCulture));
             yield return new KeyValuePair<string, string>("Version", fullVersionValue);
             yield return new KeyValuePair<string, string>(WellKnownVariables.Version, fullVersionValue);
+        }
+
+        private static bool ValidateVersionNumber(KeyValuePair<string, string> s)
+        {
+            if (string.IsNullOrWhiteSpace(s.Value))
+            {
+                return false;
+            }
+
+            int parsed;
+
+            if (!int.TryParse(s.Value, out parsed) || parsed < 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public int Order => VariableProviderOrder.Ignored;
