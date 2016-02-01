8eb25b9490a14ea1e2b0f7cbf5bc2fc7276f4d95
Niklas Lundberg <info@nlundberg.se>
2016-02-01 14:14:56 +0100

    Added logging in build versison provider.

--- src/Arbor.X.Core/Tools/Versioning/BuildVersionProvider.cs
+++ src/Arbor.X.Core/Tools/Versioning/BuildVersionProvider.cs
@@ -24,7 +24,7 @@ namespace Arbor.X.Core.Tools.Versioning
         public Task<IEnumerable<IVariable>> GetEnvironmentVariablesAsync(ILogger logger, IReadOnlyCollection<IVariable> buildVariables, CancellationToken cancellationToken)
         {
             IEnumerable<KeyValuePair<string, string>> variables =
-                GetVersionVariables(buildVariables);
+                GetVersionVariables(buildVariables, logger);
 
             List<EnvironmentVariable> environmentVariables = variables
                 .Select(item => new EnvironmentVariable(item.Key, item.Value))
@@ -34,77 +34,158 @@ namespace Arbor.X.Core.Tools.Versioning
         }
 
         IEnumerable<KeyValuePair<string, string>> GetVersionVariables(
-            IReadOnlyCollection<IVariable> buildVariables)
+            IReadOnlyCollection<IVariable> buildVariables, ILogger logger)
         {
 
             var environmentVariables =
                 buildVariables.Select(item => new KeyValuePair<string, string>(item.Key, item.Value)).ToList();
 
-            int major =
-                environmentVariables.Where(item => item.Key == WellKnownVariables.VersionMajor)
-                    .Select(item => (int?) int.Parse(item.Value))
-                    .SingleOrDefault() ?? -1;
-            int minor =
-                environmentVariables.Where(item => item.Key == WellKnownVariables.VersionMinor)
-                    .Select(item => (int?) int.Parse(item.Value))
-                    .SingleOrDefault() ?? -1;
-            int patch =
-                environmentVariables.Where(item => item.Key == WellKnownVariables.VersionPatch)
-                    .Select(item => (int?) int.Parse(item.Value))
-                    .SingleOrDefault() ?? -1;
-            int build =
-                environmentVariables.Where(item => item.Key == WellKnownVariables.VersionBuild)
-                    .Select(item => (int?) int.Parse(item.Value))
-                    .SingleOrDefault() ?? -1;
+            int major = -1;
+            int minor = -1;
+            int patch = -1;
+            int build = -1;
 
             string sourceRoot = buildVariables.GetVariable(WellKnownVariables.SourceRoot).ThrowIfEmptyValue().Value;
 
-            var versionFileName = Path.Combine(sourceRoot, "version.json");
+            var fileName = "version.json";
+
+            var versionFileName = Path.Combine(sourceRoot, fileName);
 
             if (File.Exists(versionFileName))
             {
-                IReadOnlyCollection<KeyValueConfigurationItem> keyValueConfigurationItems = new JsonFileReader(versionFileName).ReadConfiguration();
-
-                var jsonKeyValueConfiguration = new JsonKeyValueConfiguration(keyValueConfigurationItems);
-
-                var majorKey = "major"; //TODO defined major key
-
-                var minorKey = "minor"; //TODO defined minor key
-
-                var patchKey = "patch"; //TODO defined patch key
-
-                var required = new Dictionary<string, string>
-                                   {
-                                       { majorKey, jsonKeyValueConfiguration.ValueOrDefault(majorKey)},
-                                       { minorKey, jsonKeyValueConfiguration.ValueOrDefault(minorKey)},
-                                       { patchKey, jsonKeyValueConfiguration.ValueOrDefault(patchKey)}
-                                   };
-
-                if (required.All(ValidateVersionNumber))
+                logger.WriteVerbose($"A version file was found with name {versionFileName} at source root '{sourceRoot}'");
+                IReadOnlyCollection<KeyValueConfigurationItem> keyValueConfigurationItems = null;
+                try
                 {
-                    major = required[majorKey].TryParseInt32().Value;
-                    minor = required[minorKey].TryParseInt32().Value;
-                    patch = required[patchKey].TryParseInt32().Value;
+                   keyValueConfigurationItems =
+                        new JsonFileReader(versionFileName).ReadConfiguration();
                 }
+                catch (Exception ex) when (!ex.IsFatal())
+                {
+                    logger.WriteWarning($"Could not read the configuration content in file '{versionFileName}'");
+                }
+
+                if (keyValueConfigurationItems != null)
+                {
+                    var jsonKeyValueConfiguration = new JsonKeyValueConfiguration(keyValueConfigurationItems);
+
+                    var majorKey = "major"; //TODO defined major key
+
+                    var minorKey = "minor"; //TODO defined minor key
+
+                    var patchKey = "patch"; //TODO defined patch key
+
+                    var required = new Dictionary<string, string>
+                                       {
+                                           {
+                                               majorKey,
+                                               jsonKeyValueConfiguration.ValueOrDefault(
+                                                   majorKey)
+                                           },
+                                           {
+                                               minorKey,
+                                               jsonKeyValueConfiguration.ValueOrDefault(
+                                                   minorKey)
+                                           },
+                                           {
+                                               patchKey,
+                                               jsonKeyValueConfiguration.ValueOrDefault(
+                                                   patchKey)
+                                           }
+                                       };
+
+                    if (required.All(ValidateVersionNumber))
+                    {
+                        major = required[majorKey].TryParseInt32().Value;
+                        minor = required[minorKey].TryParseInt32().Value;
+                        patch = required[patchKey].TryParseInt32().Value;
+
+                        logger.WriteVerbose(
+                            $"All version numbers from the version file '{versionFileName}' were parsed successfully");
+                    }
+                    else
+                    {
+
+                        logger.WriteVerbose(
+                            $"Not all version numbers from the version file '{versionFileName}' were parsed successfully");
+                    }
+                }
+
+            }
+            else
+            {
+                logger.WriteVerbose($"No version file found with name {versionFileName} at source root '{sourceRoot}' was found");
+            }
+
+            int envMajor =
+                environmentVariables.Where(item => item.Key == WellKnownVariables.VersionMajor)
+                    .Select(item => (int?)int.Parse(item.Value))
+                    .SingleOrDefault() ?? -1;
+            int envMinor =
+                environmentVariables.Where(item => item.Key == WellKnownVariables.VersionMinor)
+                    .Select(item => (int?)int.Parse(item.Value))
+                    .SingleOrDefault() ?? -1;
+            int envPatch =
+                environmentVariables.Where(item => item.Key == WellKnownVariables.VersionPatch)
+                    .Select(item => (int?)int.Parse(item.Value))
+                    .SingleOrDefault() ?? -1;
+            int envBuild =
+                environmentVariables.Where(item => item.Key == WellKnownVariables.VersionBuild)
+                    .Select(item => (int?)int.Parse(item.Value))
+                    .SingleOrDefault() ?? -1;
+
+            if (envMajor >= 0)
+            {
+                logger.WriteVerbose($"Found major {envMajor} version in build variable");
+                major = envMajor;
+            }
+
+            if (envMinor >= 0)
+            {
+                logger.WriteVerbose($"Found minor {envMinor} version in build variable");
+                minor = envMinor;
+            }
+
+            if (envPatch >= 0)
+            {
+                logger.WriteVerbose($"Found patch {envPatch} version in build variable");
+                patch = envPatch;
+            }
+
+            if (envBuild >= 0)
+            {
+                logger.WriteVerbose($"Found build {envBuild} version in build variable");
+                build = envBuild;
             }
 
             if (major < 0)
+
             {
+                logger.WriteVerbose($"Found no major version, using version 0");
                 major = 0;
             }
 
             if (minor < 0)
             {
+                logger.WriteVerbose($"Found no minor version, using version 0");
                 minor = 0;
             }
 
             if (patch < 0)
             {
+                logger.WriteVerbose($"Found no patch version, using version 0");
                 patch = 0;
+            }
+
+            if (build < 0)
+            {
+                logger.WriteVerbose($"Found no build version, using version 0");
+                build = 0;
             }
 
             if (major == 0 && minor == 0 && patch == 0)
             {
+                logger.WriteVerbose("Major minor and build versions are all 0, setting minor version to 1");
                 minor = 1;
             }
 
