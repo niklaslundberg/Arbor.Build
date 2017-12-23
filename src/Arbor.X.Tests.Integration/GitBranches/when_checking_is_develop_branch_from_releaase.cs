8eb41463b60f0b6fc6346e11e98df661ab40556c
Niklas Lundberg <info@nlundberg.se>
2017-12-24 00:02:06 +0100

    Added branch name tests.

---
+++ src/Arbor.X.Tests.Integration/GitBranches/when_checking_is_develop_branch_from_releaase.cs
@@ -0,0 +1,17 @@
+﻿using Arbor.X.Core.Tools.Git;
+using Machine.Specifications;
+
+namespace Arbor.X.Tests.Integration.GitBranches
+{
+    [Subject(typeof(BranchHelper))]
+    public class when_checking_is_develop_branch_from_releaase
+    {
+        static BranchName branchName;
+        static bool is_develop;
+        Establish context = () => { branchName =new BranchName("refs/heads/release-1.2.3"); };
+
+        Because of = () => { is_develop = branchName.IsDevelopBranch();};
+
+        It should_extract_the_version = () => is_develop.ShouldBeFalse();
+    }
+}
---
+++ src/Arbor.X.Tests.Integration/GitBranches/when_checking_is_production_branch_from_releaase.cs
@@ -0,0 +1,17 @@
+﻿using Arbor.X.Core.Tools.Git;
+using Machine.Specifications;
+
+namespace Arbor.X.Tests.Integration.GitBranches
+{
+    [Subject(typeof(BranchHelper))]
+    public class when_checking_is_production_branch_from_releaase
+    {
+        static BranchName branchName;
+        static bool is_production;
+        Establish context = () => { branchName =new BranchName("refs/heads/release-1.2.3"); };
+
+        Because of = () => { is_production = branchName.IsProductionBranch();};
+
+        It should_extract_the_version = () => is_production.ShouldBeTrue();
+    }
+}
