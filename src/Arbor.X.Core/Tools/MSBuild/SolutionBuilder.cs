651293229f304ce22b4d574065dd7157cf99a962
Niklas Lundberg <info@nlundberg.se>
2018-11-06 21:28:50 +0100

    Added restore for runtime identifier publications.

--- src/Arbor.X.Core/Tools/MSBuild/SolutionBuilder.cs
+++ src/Arbor.X.Core/Tools/MSBuild/SolutionBuilder.cs
@@ -1492,7 +1492,7 @@ namespace Arbor.Build.Core.Tools.MSBuild
                 if (!string.IsNullOrWhiteSpace(rid))
                 {
                     buildSiteArguments.Add($"/property:RuntimeIdentifier={rid}");
-                    target = "rebuild;publish";
+                    target = "restore;rebuild;publish";
                 }
                 else
                 {
