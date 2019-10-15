ba79b8dd78503ef7e0ec10aec23d1ce3f5b9096c
Niklas Lundberg <info@nlundberg.se>
2019-10-15 22:13:19 +0200

    More test cleanup.

--- src/Arbor.X.Tests.Integration/ProcessRunner/when_running_process_without_logging.cs
+++ src/Arbor.X.Tests.Integration/ProcessRunner/when_running_process_without_logging.cs
@@ -9,22 +9,22 @@ namespace Arbor.Build.Tests.Integration.ProcessRunner
     [Subject(typeof(Processing.ProcessRunner))]
     public class when_running_process_without_logging
     {
-        Because of = () => { RunAsync().Wait(TimeSpan.FromMilliseconds(5000)); };
         static ExitCode exitCode;
 
-
-        private static async Task RunAsync() => exitCode =
-                await
-                    Processing.ProcessRunner.ExecuteProcessAsync(@"C:\Windows\System32\PING.EXE",
-                        arguments: new[] { "127.0.0.1" },
-                        standardOutLog: null,
-                        standardErrorAction: null,
-                        toolAction: null,
-                        verboseAction: null,
-                        debugAction: null,
-                        noWindow: true,
-                        cancellationToken: CancellationToken.None).ConfigureAwait(false);
+        Because of = () => { RunAsync().Wait(TimeSpan.FromMilliseconds(5000)); };
 
         It should_return_exit_code_0 = () => { exitCode.ShouldEqual(ExitCode.Success); };
+
+        static async Task RunAsync() => exitCode =
+            await
+                Processing.ProcessRunner.ExecuteProcessAsync(@"C:\Windows\System32\PING.EXE",
+                    new[] { "127.0.0.1" },
+                    (s, s1) => { },
+                    null,
+                    null,
+                    null,
+                    debugAction: null,
+                    noWindow: true,
+                    cancellationToken: CancellationToken.None).ConfigureAwait(false);
     }
 }
