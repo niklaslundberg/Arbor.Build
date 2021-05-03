9540dc1b8d4f9c6f76bcf2cf5290c72b99f858da
Niklas Lundberg <info@nlundberg.se>
2021-05-03 21:35:47 +0200

    Fixed enumerableof.

--- src/Arbor.Defensive.Collections/EnumerableOf.cs
+++ src/Arbor.Defensive.Collections/EnumerableOf.cs
@@ -7,13 +7,13 @@ namespace Arbor.Defensive.Collections
 {
     public static class EnumerableOf<T> where T : class
     {
-        private static readonly Lazy<ImmutableArray<T>> LazyEnumerable = new Lazy<ImmutableArray<T>>(Initialize);
+        private static readonly Lazy<ImmutableArray<T>> LazyEnumerable = new(Initialize);
 
         public static ImmutableArray<T> Items => LazyEnumerable.Value;
 
         private static ImmutableArray<T> Initialize() =>
             typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static)
-                .Where(field => field.IsInitOnly && field.GetType() == typeof(T))
+                .Where(field => field.IsInitOnly && field.FieldType == typeof(T))
                 .Select(field => field.GetValue(null) as T)
                 .NotNull()
                 .ToImmutableArray();
---
+++ tests/Arbor.Build.Tests.Integration/Collections/EnumerableOfTests.cs
@@ -0,0 +1,24 @@
+﻿using Arbor.Defensive.Collections;
+using FluentAssertions;
+using Xunit;
+
+namespace Arbor.Build.Tests.Integration.Collections
+{
+    public class EnumerableOfTests
+    {
+        [Fact]
+        public void EnumerableOfClassWith3PublicReadOnlyFieldsShouldList3Items()
+        {
+            EnumerableOf<EnumerableTestClass>.Items.Should().HaveCount(3);
+        }
+
+        private class EnumerableTestClass
+        {
+            public static readonly EnumerableTestClass A = new();
+            public static readonly EnumerableTestClass B = new();
+            public static readonly EnumerableTestClass C = new();
+            public static readonly string D = "Other";
+            public static readonly EnumerableTestClass? E = null;
+        }
+    }
+}
