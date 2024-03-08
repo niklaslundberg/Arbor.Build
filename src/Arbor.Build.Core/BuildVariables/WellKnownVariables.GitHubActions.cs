6ccf515e9d87388ad04f26c94899c4cce4c25e2b
Niklas Lundberg <info@nlundberg.se>
2024-03-08 22:01:39 +0100

    Add GitHub actions as build server.

---
+++ src/Arbor.Build.Core/BuildVariables/WellKnownVariables.GitHubActions.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace Arbor.Build.Core.BuildVariables;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "StyleCop.CSharp.NamingRules",
+    "SA1310:Field names must not contain underscore",
+    Justification = "Variables")]
+public partial class WellKnownVariables
+{
+    [VariableDescription("Jenkins HOME path")]
+    public const string ExternalTools_GitHubActions =
+        "GITHUB_ACTIONS";
+}
